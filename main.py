9760d642ad819e762c30f1d3d5edc49376f7fb61
45Sanabria <45Sanabria@gmail.com>
2018-04-08 13:51:54 -0400

    Add files via upload
    positive test result adding input

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import mysql.connector
 
-cnx = mysql.connector.connect(user = 'root', password= 'learning', host = '127.0.0.1', database = 'employees')
+
+
+config = {
+  'user': 'root',
+  'password': 'learning',
+  'host': '127.0.0.1',
+  'database': 'journal',
+  'raise_on_warnings': True,
+  'use_pure': False,
+}
+
+
+cnx = mysql.connector.connect(**config)
+cursor = cnx.cursor()
+
+entry = ("INSERT INTO test (num, entry) VALUES (001, 'This is a test')")
+
+cursor.execute(entry)
+
+cnx.commit()
+cursor.close()
 
 cnx.close()
